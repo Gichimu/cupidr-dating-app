377cbf7baf51214eab087a9d15eb1b0c429a645b
Victorvick399 <victorvick399@gmail.com>
2019-12-04 17:46:46 +0300

    enable sign in and sign up functions

--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ from flask_login import UserMixin
 from . import db, login_manager
 from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class User(UserMixin, db.Model):
     '''
     Class that defines the user object template
--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@ from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Quality
-# from app.models import User, Comment, Blogpost
 
 #create app instance
 app = create_app('development')
