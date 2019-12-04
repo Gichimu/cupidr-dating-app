d97cb0c2b22cf0f0608108c0084baa3260aadc52
Gichimu <gichimueric12@gmail.com>
2019-12-04 09:14:56 +0300

    configure manage.py

---
+++ manage.py
@@ -0,0 +1,18 @@
+from app import create_app
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+# from app.models import User, Comment, Blogpost
+
+#create app instance
+app = create_app('production')
+manager = Manager(app)
+manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app)
+
+if __name__ == '__main__':
+    manager.run()
